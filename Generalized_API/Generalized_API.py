6d6cfd7706ad76acd204d2b6c9b97f4aebeb4199
Brandon Alexandeer Roemer <brro7863@colorado.edu>
2019-12-09 13:38:27 -0700

    Moved PI

--- Generalized_API/Generalized_API.py
+++ Generalized_API/Generalized_API.py
@@ -11,14 +11,14 @@ url = "https://itll-demeter.int.colorado.edu/api/v1/hardware"
 
 
 #Read in Config
-f = open("API_KEY.txt", "r")
+f = open("../../API_KEY.key", "r")
 API_KEY = str(f.read())[:-1]
 
 #Format Request
 AUTH = "Bearer " + API_KEY
 
 catagories = ["57"]
-for i in catagories:
+for j in catagories:
     querystring = {"limit":"1",
                         "offset":"0",
                         "category_id": j
@@ -35,14 +35,14 @@ for i in catagories:
 
     #Convert to Dictionary
     y = json.loads(response.text)
-
+    """
     for item in y["rows"]:
         for comp in companies:
             for var in vars:
                 if(len(var[i]) == 3):
                     for item[var][var][var]:
-                        if
-
+                        print("d")
+    """
           
 
 
